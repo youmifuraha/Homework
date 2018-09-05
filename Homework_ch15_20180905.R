492e9ada4c38b076dbb0a8419dce6e3c5d7f4be6
youmifuraha <youmifuraha7@gmail.com>
2018-09-05 18:00:29 +0900

    20180905

--- Homework_ch15_20180905.R
+++ Homework_ch15_20180905.R
@@ -1,77 +1,136 @@
-#
+# Homework_ch15 것!
 
-# 자주 실수하는 것!
+# 15-1 R내장 함수로 데이터 추출
 
+# 데이터 준비
 exam = read.csv("csv_exam.csv")
-exam[]
-# 행과 열을 다룰 때는 무조건 대괄호 [] [행,열]
+exam[]  # = exam 같음
 
+## 행으로 추출
 # 1행만 추출
 exam[1,]
-exam[exam$class == 1,]
 
-exam[exam$math >= 80,]
+# 2행 추출
+exam[2,]
 
-twitter = read.csv("twitter.csv")
-twitter
+# 1반인 칭구들만
+exam[exam$class == 1,]  # 행에 해당하는 조건이니깐, 쉼표 앞에만 조건 부여!
 
-exam[exam$class == 1 & exam$math >= 50]
-exam[exam$english < 90 | science <50]
+# 수학점수 80 이상
+exam[exam$math >= 80,]  # 콤마빼먹지 말긔!!
 
-exam[,1]
+# 1반이면서 수학점수 50 이상
+exam[exam$class == 1 & exam$math >= 50,]
+# 콤마빼먹지 말긔!!!!!!
 
+# 영어점수 90 미만 이거나 과학점수 50점 미만
+exam[exam$english < 90 | exam$science < 50,]
 
+## 열 변수 추출
+
+# 첫번째 열만
+exam[,1]  # id 였지!
+
+# 두번째 열만
+exam[,2]  # class 였지!
+
+# 세번쨰 열만
+exam[,3]  # math 점수
+
+### 근데 변수명이 같이 안 출력되서 불편하구만...
+
+# 변수명으로 변수 추출하기
+
+# class 변수
 exam[,"class"]
+ 
+# math 변수
 exam[,"math"]
 
-# 열 쪽은 일일이 세기 뭐하니깐, 이름으로 찾는게 좋으다.
+### 참고. 행 값 추출할 때는 '원본데이터$변수명'식으로 추출해야되는디, 열 값(변수) 추출할 때는 걍 "변수명"이구만.
 
+# class, math, english 추출
+exam[,c("class", "math", "english")]
+# c로 묶어서 출력!!
+
+# 열 쪽은 일일이 세기 뭐하니깐, 이름으로 찾는게 좋으다!!
+
+## 행, 열 동시 추출
+
+# 1행 3열?
+exam[1,3]
+
+# 5행의 영어점수?
+exam[5,"english"]
+
+# 수학점수 50 이상 친구들의 수학 점수랑 영어 점수 -> 아니네!
 exam[exam$math >= 50, "english"]
+### 어허.... 이게 아니네. 
+### 수학 점수 50 이상 친구들의 영어점수네!
+### 수학 점수는 안 나옴.
+### 걍 수학 점수는 조건으로 만됨. 수학점수는 조건만족시키는 행이되는거고, 값은 열값인 english 점수가 나옴.
+
+# 수학점수 50 넘는 친구들의 영어, 과학 점수
+exam[exam$math >= 50, c("english", "science")]
+
 exam
 
 exam[exam$math >= 50, c("english", "science")]
+# 자리 바꾸면?
+exam[exam$math >= 50, c("science", "english")]
+# 바꿔서 출력
 
-# 지금까지는 그냥 내장해서 뽑아본거죵
+# 수학 추가하면?
+exam[exam$math >= 50, c("math", "science", "english")]
+# 수학 점수도 포함해서 출력~!
 
-# dplyr 로 해보쟝
 
-install.packages("dplyr")
+## dplyr랑 비교해보장
+
 library(dplyr)
 
-# 아래 두개 코드 방법 비교!
-exam$tot = (exam$math + exam$english +exam$science)/3
-aggregate(data = exam[exam$math >= 50 & exam$english >= 80,], tot~class, mean) # 요게 group_by
+# 수학 점수 50이상, 영어 점수 80이상, 각 반의 전 과목 총평균?
 
+# 내장함수 방법
+exam$aver = (exam$math + exam$english +exam$science)/3
+aggregate(data = exam[exam$math >= 50 & exam$english >= 80,], aver~class, mean)
+# aggregate 함수는 dplyr 에서 group_by 같은 칭구. data는 주어진 주건에 만족하는 애들만 씀. 그리고, aver~class: 반에따른 평균!, mean = 평균값 구해줘 함수.
+
+# dplyr 방법
 exam %>% 
   filter(math >= 50 & english >= 80) %>% 
-  mutate(tot = (math + english + science)/3) %>% 
+  mutate(aver2 = (math + english + science)/3) %>% 
   group_by(class) %>% 
-  summarise(mean = mean(tot))
+  summarise(mean = mean(aver2))
 
 # dplyr 하는게 더 직관적! 글구 내장꺼는 dataset안에 변수 쓸 때마다 그 dataset이름하고 $변수 해줘야되서 더 복잡함!
 
 
 # 혼자서 해보기
 
-install.packages("ggplot2")
 library(ggplot2)
-mpg = as.data.frame(ggplot2::mpg)
 mpg
 
 mpg$total = (mpg$cty + mpg$hwy) / 2
 mpg
 
-mpg[mpg$class == c("compact, suv"),]
+# 통합연비변수 생성
+mpg$total = (mpg$cty + mpg$hwy) / 2
 
 # compact 추출
 df_comp = mpg[mpg$class == "compact",]
+
+# suv 추출
 df_suv = mpg[mpg$class == "suv",]
 
+# 각 평균값
 mean(df_comp$total)
 mean(df_suv$total)
 
-library(dplyr)
 
+## dplyr 로 구해보기
+
+library(dplyr)
 
 mpg %>%
   mutate(aver = (cty + hwy) / 2) %>% 
@@ -79,56 +138,21 @@ mpg %>%
   group_by(class) %>% 
   summarise(mean = mean(aver))
 
-# 오홋! 옆자리 친구가 알려줌 ㅎ.ㅎ
+# 간단&직관적
 
 
-
-
-# 변수 타입
-
-# 함수마다 적용 가능한 변수 타입이 다를 수 있다.
-# 분석 전에 변수 타입 무엇인지 확인 필요
-# 함수 실행시 오류가 발생하거나 예상과 다른 결과가 출력되면 변수 타입 확인할 것!!
-
-# Satoshi Nakamoto : 혜성과 같은 사람. 
-# 갑자기 논문 1편(2009, 9장짜리)(예고도 없이) 발표했는데 10년만에 터짐. 세상을 뒤짚어높음.
-# 근데 실체가 없음. 기자들도 못 찾음.
-# 2000만원까지 만들어놨다가 지금은 800만원.
-# 이 사람이 겁나 많이(200만) 갖고 있는데 한번도 안 씀. 전혀 정체를 들어내지 않음. 
-# 1.의심가는 사람있는데... 죽어서? 2.비밀번호를 찾고있다?
-# block chain이란?
-# 사람사이에 돈거래를 하고 싶은데, 그냥 하기 믿기 어려우니깐, 은행이 하는데. 은행이 수수료 등 막 떼.
-# 은행 수수료 아까우니깐 컴퓨터가 하게끔 하는게 block chain
-# 누구나 확인가능하니깐 해킹하려면 모든 사람 해킹해야해서 할 수가 없당.
-# 홈페이지에 있으니깐 참고해봐영.
-# 블록체인은 지금 완전 블루오션이야요. 지금이 시작임.
-# 무궁무진해영.
-# 장담하건데, 지금 인터넷 컴퓨터 상상할 수 없듯이, 없으면 말이 안 되듯이,
-# 블록체인도 그렇게 될 것이다!!!!
-# 관심을 가져봐용. 블록체인의 철학!!! 뺏긴 권력을 되찾자!!!
-# Bitcoin: A Peer-to-Peer Electronic Cash System
-# Satoshi Nakamoto 라는게 아마 가명인듯. 일본이 그래서 일본사람인줄알고 겁나 지원하줌.
-# 작년에 한국에서 그걸(?) 가져올라고 했는데... 말아먹음.
-
-# SNU Coin ㅋㅋㅋㅋ 선생님이 만든거ㅋㅋㅋ 그걸로 상 받으셨다능.
-
-
-
-
-# ch15.
-# 변수 타입
+## 변수 타입
 
 # 연속 변수 vs 범주 변수
-# 연속 변수 : Numeric 타입! 값에 의미가 있다. 연산이 가능하다. ex.키, 몸무게
-# 범주 변수 : Factor 타입! 검증. 문자로 대체 가능. 숫자의 크기와 전혀 상관이 없다. ex.남녀(1,2), 지역(1,2,3,4)
 
-var1 = c(1, 2, 3, 1, 2)
-var2 = factor(c(1, 2, 3, 1, 2))
+var1 = c(1, 2, 3, 1, 2)  # 연속
+var2 = factor(c(1, 2, 3, 1, 2))  # 범주
 
 var1
 var2
 
 var1+2  # 계산이 된당.
+
 # var2+2
 # Warning message: 
 # In Ops.factor(var2, 2) : 요인(factors)에 대하여 의미있는 ‘+’가 아닙니다.
@@ -166,6 +190,7 @@ class(mpg$drv)
 mpg$drv = as.factor(mpg$drv)
 levels(mpg$drv)
 
+
 ## 15-3 데이터 구조
 
 # 스칼라 : 하나
@@ -184,10 +209,6 @@ class(x1)
 x2 = matrix(c(1:12), ncol = 2)
 x2
 class(x2)
-# 매트릭스 가장 큰 차이점!
-# 데이터프레임은 여러가지 형태를 구성할 수 있는데,
-# 매트릭스는 한가지만 가능하다!(2차원이긴 하지만)
-# 그래서 매트릭스 별로 안 쓰고, 데이터프레임을 써.
 
 # array 만들기 1~20으로 2행*5열*2차원
 # 2차원 이상!!
@@ -196,8 +217,7 @@ x3 = array(1:20, dim = c(2, 5, 2))
 x3
 class(x3)
 
-# so, 제일 만만한게 데이터프레임이다~!
-# ㅋㅋㅋ 데이터프레임이 진리다.
+# so, 제일 만만한게 데이터프레임!!
 
 # 리스트!!
 # 모든 데이터구조를 죄다 포함한다.(벡터, 어레이, 리스트, 데이터프레임 등등등)
@@ -232,8 +252,3 @@ i
 i$stats[,1]
 i$stats[,1][4] # 3 분위! 5개 중에서는 4번째죵
 i$stats[,1][3] # 요게 중앙값일꺼규용~~
-
-# 그래서 오늘은 실수하는거 봤어용~
-# 근데 그중에서도 실수 많이 하는 것은 주로, 오타!!!!!!!! 랑 설치 안 한거!!!!!!!!!!
-
-# 내일은 공짜로 R공부하는 사이트, 자료 받는 곳 등등 알려준데용. ㅋㅋ 처음을 같이 해 봅시당. 가입도! 그래서 앞으로 계속 공부할 수 있도록.!!
